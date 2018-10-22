621ee50db1009bcf987fa61189ef2f386ad1bda2
Trevor Coehoorn <tcoehoorn@impetusdigital.com>
2018-10-22 12:36:13 -0600

    Clear cache before running updb

--- update_site.sh
+++ update_site.sh
@@ -51,7 +51,7 @@ gzip $sql_file
 
 cd ~/work/impetus/$1
 
-docker exec docker_$1_1 drush updb -y
 docker exec docker_$1_1 drush cc all
+docker exec docker_$1_1 drush updb -y
 
 php ./private/scripts/disable_scheduled_emails.php
