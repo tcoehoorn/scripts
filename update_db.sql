937d6007502afd651d52b1339ce499b0c215b600
Trevor Coehoorn <tcoehoorn@impetusdigital.com>
2019-02-11 11:05:16 -0700

    Grant permission for Drupal user from multiple hosts
    Necessary to run drush commands from docker host

--- update_db.sql
+++ update_db.sql
@@ -1,6 +1,6 @@
 
 DROP DATABASE IF EXISTS `impetusmaster`;
 CREATE DATABASE `impetusmaster`;
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON `impetusmaster`.*  TO 'impetusmaster'@'localhost' IDENTIFIED BY 'impetustest';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON `impetusmaster`.*  TO 'impetusmaster'@'%' IDENTIFIED BY 'impetustest';
 USE `impetusmaster`;
 SOURCE impetusmaster.sql;
