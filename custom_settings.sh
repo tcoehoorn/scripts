345c2555b5fc0cac42625e4d75d8ddaaf20534ef
Trevor Coehoorn <tcoehoorn@impetusdigital.com>
2019-05-06 11:41:06 -0600

    Remove "comment out" statement

--- custom_settings.sh
+++ custom_settings.sh
@@ -2,6 +2,5 @@
 
 settings_file="./sites/default/settings.php"
 
-vim -c "70,83s/^/\/\/ /" -c "wq" $settings_file
 echo "include_once('local.settings.php');" >> $settings_file
 git update-index --assume-unchanged $settings_file
