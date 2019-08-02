67bb3f442ee8b01ab53afe1f6932041283c9ab76
Trevor Coehoorn <tcoehoorn@impetusdigital.com>
2019-08-02 11:47:41 -0600

    Hard code ip address and dev site domain

--- blocal.sh
+++ blocal.sh
@@ -1,13 +1,8 @@
 #! /bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: blocal.sh <url>"
-    exit
-fi
-
 FILE="./build/behat/behat.yml"
 
-sed -i "/wd_host/s/localhost/ambrose/" $FILE
+sed -i "/wd_host/s/localhost/10.0.0.185/" $FILE
 sed -i "/javascript_session/s/browser_stack/selenium2/" $FILE
-sed -i "/base_url/s/\".*\"/\"http:\/\/www.$1\"/" $FILE
+sed -i "/base_url/s/\".*\"/\"https:\/\/iip.trdv.site\"/" $FILE
 git update-index --assume-unchanged $FILE
