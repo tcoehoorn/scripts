6cf3fec18597dc532e7e00e60f4e5391d0c93bee
Trevor Coehoorn <tcoehoorn@impetusdigital.com>
2020-03-20 20:22:07 +0100

    Get the ip address value dynamically

--- blocal.sh
+++ blocal.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 FILE="./build/behat/behat.yml"
+IP_ADDR=$(ipconfig | grep -m 1 "IPv4 Address" | awk '{print $NF}' | tr -d "\n")
 
-sed -i "/wd_host/s/localhost/10.0.0.185/" $FILE
+sed -i "/wd_host/s/localhost/${IP_ADDR%%[[:space:]]}/" $FILE
 sed -i "/javascript_session/s/browser_stack/selenium2/" $FILE
 sed -i "/base_url/s/\".*\"/\"https:\/\/iip.trdv.site\"/" $FILE
 git update-index --assume-unchanged $FILE
