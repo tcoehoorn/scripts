d6eaac88283159742417fe7556325a4c9c82d5e9
Trevor Coehoorn <tcoehoorn@impetusdigital.com>
2020-08-27 19:24:00 +0100

    Remove temp dir from vagrant machine after files have been downloaded

--- download_cachegrid.sh
+++ download_cachegrid.sh
@@ -12,6 +12,7 @@ pushd $WORK_DIR/vagrant
 vagrant ssh -c "rm -rf /home/vagrant/tmp/tmp; docker cp docker_portal_1:/tmp /home/vagrant/tmp/"
 rm -rf $CACHEGRIND_DIR
 vagrant scp default:/home/vagrant/tmp/tmp $(cygpath -d "$WORK_DIR/tmp") 
+vagrant ssh -c "rm -rf /home/vagrant/tmp/tmp"
 mv $WORK_DIR/tmp/tmp $CACHEGRIND_DIR
 
 popd
