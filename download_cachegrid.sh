7b2e9492d44ef1c2a77cf314f39d34e055cd021b
Trevor Coehoorn <tcoehoorn@impetusdigital.com>
2020-08-27 19:21:03 +0100

    Create script to download cachegrind files

---
+++ download_cachegrid.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#
+# Download xdebug cachegrind files
+#
+
+WORK_DIR=/cygdrive/c/Users/tcoeh/work/impetus
+CACHEGRIND_DIR=$WORK_DIR/tmp/cachegrind
+
+pushd $WORK_DIR/vagrant
+
+vagrant ssh -c "rm -rf /home/vagrant/tmp/tmp; docker cp docker_portal_1:/tmp /home/vagrant/tmp/"
+rm -rf $CACHEGRIND_DIR
+vagrant scp default:/home/vagrant/tmp/tmp $(cygpath -d "$WORK_DIR/tmp") 
+mv $WORK_DIR/tmp/tmp $CACHEGRIND_DIR
+
+popd
