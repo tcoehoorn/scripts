537d9f303a51419377ace461a0561adaf5cc66b5
Trevor Coehoorn <tcoehoorn@impetusdigital.com>
2021-10-14 09:48:19 -0600

    Update to script to handle sites that are run via docker

--- blocal.sh
+++ blocal.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
 FILE="./build/behat/behat.yml"
-IP_ADDR=$(ipconfig | grep -m 1 "IPv4 Address" | awk '{print $NF}' | tr -d "\n")
+IP_ADDR=$(ipconfig.exe | grep -m 1 "IPv4 Address" | awk '{print $NF}' | tr -d "\n")
 
 sed -i "/wd_host/s/localhost/${IP_ADDR%%[[:space:]]}/" $FILE
 sed -i "/javascript_session/s/browser_stack/selenium2/" $FILE
 sed -i "/base_url/s/\".*\"/\"https:\/\/iip.trdv.site\"/" $FILE
+sed -i "/files_path/s/'.*'/\'\/var\/www\/html\/build\/behat\/files\'/" $FILE
+sed -i "/root/s/'.*'/\'\/var\/www\/html\'/" $FILE
+sed -i "/binary/s/'.*'/\'\/var\/www\/html\/build\/behat\/bin\/drush\'/" $FILE
 git update-index --assume-unchanged $FILE
